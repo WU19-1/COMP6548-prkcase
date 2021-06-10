3bf7930ee99fae19052f61827f986053c6285f77
WU19-1 <wahyu.mai@gmail.com>
2021-06-11 01:30:31 +0700

    testing and code correction

--- portscanner.py
+++ portscanner.py
@@ -1,7 +1,7 @@
 from re import L
 from scapy.all import *
 from socket import socket
-import getopt
+from getopt import getopt
 from threading import Thread, Event, current_thread
 import sys
 
@@ -11,6 +11,7 @@ OPEN_PORTS = []
 FILTERED_PORTS = []
 TIMEOUT = 3
 ICMP_UNREACHABLE_ERROR = [1,2,3,9,10,13]
+conf.verb = 0
 
 # https://scapy.readthedocs.io/en/latest/api/scapy.layers.inet.html
 
@@ -23,6 +24,7 @@ def xmas(ip, start, end, step):
             if resp.haslayer(ICMP):
                 if int(resp.haslayer(ICMP).type) == 3 and int(resp.haslayer(ICMP).code) in ICMP_UNREACHABLE_ERROR:
                     FILTERED_PORTS.append(port)
+    THREAD_LIST.remove(current_thread())
 
 def null(ip, start, end, step):
     for port in range(start, end, step):
@@ -33,6 +35,7 @@ def null(ip, start, end, step):
             if resp.haslayer(ICMP):
                 if int(resp.haslayer(ICMP).type) == 3 and int(resp.haslayer(ICMP).code) in ICMP_UNREACHABLE_ERROR:
                     FILTERED_PORTS.append(port)
+    THREAD_LIST.remove(current_thread())
 
 def fin(ip, start, end, step):
     for port in range(start, end, step):
@@ -43,14 +46,17 @@ def fin(ip, start, end, step):
             if resp.haslayer(ICMP):
                 if int(resp.haslayer(ICMP).type) == 3 and int(resp.haslayer(ICMP).code) in ICMP_UNREACHABLE_ERROR:
                     FILTERED_PORTS.append(port)
+    THREAD_LIST.remove(current_thread())
 
 def syn(ip, start, end, step):
     for port in range(start, end, step):
         s = socket()
         s.settimeout(3)
         res = s.connect_ex((ip, port))
+        print(port)
         if res == 0:
             OPEN_PORTS.append(port)
+    THREAD_LIST.remove(current_thread())
 
 def stealthsyn(ip, start, end, step):
     for port in range(start, end, step):
@@ -58,13 +64,14 @@ def stealthsyn(ip, start, end, step):
         if type(resp).__name__ == 'NoneType':
             FILTERED_PORTS.append(port)
         elif resp.haslayer(TCP):
-            if resp.haslayer(TCP).flags == 0x12:
+            if resp.getlayer(TCP).flags == 0x12:
                 # send rst packet, cancel SYN packet
                 sr1(IP(dst=ip)/TCP(dport=port,flags="R"), timeout=TIMEOUT)
                 OPEN_PORTS.append(port)
             elif resp.haslayer(ICMP):
-                if int(resp.haslayer(ICMP).type) == 3 and int(resp.haslayer(ICMP).code) in ICMP_UNREACHABLE_ERROR:
+                if int(resp.getlayer(ICMP).type) == 3 and int(resp.getlayer(ICMP).code) in ICMP_UNREACHABLE_ERROR:
                     FILTERED_PORTS.append(port)
+    THREAD_LIST.remove(current_thread())
 
 def checker():
     while len(THREAD_LIST) != 0:
@@ -73,7 +80,7 @@ def checker():
         event.set()
 
 def main():
-    options, _ = getopt(sys.argv[1:], "i:s:e:t:", ["--ip=", "--startPort=", "--endPort=", "--thread=", "--xmas",  "--fin", "--null", '--syn', '--stealthsyn'])
+    options, _ = getopt(sys.argv[1:], "i:s:e:t:", ["ip=", "startPort=", "endPort=", "thread=", "xmas",  "fin", "null", 'syn', 'stealthsyn'])
     mode = -1
     count = 0
     for k, v in options:
@@ -96,7 +103,10 @@ def main():
                 mode = 3
             elif k == '--stealthsyn':
                 mode = 4
-    
+    # print("Mode",mode)
+    # print("start ", start, (start > 0 and start < 65536))
+    # print("end", end, (end > 0 and end < 65536 and end >= start))
+
     scan_mode = {
         0 : xmas,
         1 : fin,
@@ -105,21 +115,29 @@ def main():
         4 : stealthsyn
     }
 
-    if mode != -1 and (start > 0 and start < 65536) and (end > 0 and end < 65536 and end > start):
+    if mode != -1 and (start > 0 and start < 65536) and (end > 0 and end < 65536 and end >= start):
+
         for begin in range(count):
             t = Thread(target=scan_mode[mode],args=(ip, begin + start, end, count,))
             THREAD_LIST.append(t)
 
             t.start()
 
+    Thread(target=checker).start()
+
     event.wait()
     
     print("Port scanning process is finish: ")
     print("Open ports : ")
-    for op in OPEN_PORTS:
-        print("[+] %d Open "%(op))
+    if mode in [0,1,2]:
+        for op in OPEN_PORTS:
+            print("[+] %d Open | Filtered"%(op))
+    else:
+        for op in OPEN_PORTS:
+            print("[+] %d Open"%(op))
+
     for fi in FILTERED_PORTS:
-        print("[+] %d Open | Filtered"%(fi))
+        print("[+] %d Filtered"%(fi))
 
 if __name__ == "__main__":
     main()
